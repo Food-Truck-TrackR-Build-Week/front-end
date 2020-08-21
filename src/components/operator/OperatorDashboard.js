79f2328cf13ed7dbafd2d140e4363f72666d4c86
slroberts <shomariroberts@gmail.comclear>
2020-08-21 14:28:35 -0400

    updated dashboard layout

--- src/components/operator/OperatorDashboard.js
+++ src/components/operator/OperatorDashboard.js
@@ -1,61 +1,37 @@
 import React from "react";
-import {connect} from "react-redux";
-import {BrowserRouter as Switch, Route, Link} from "react-router-dom";
+// import {connect} from "react-redux";
+// import {BrowserRouter as Switch, Route, Link} from "react-router-dom";
 
+import Header from "../../components/Header";
 import AddTruckForm from "./AddTruckForm";
+import TruckList from "./TruckList";
 import FoodTruck from "./FoodTruck";
-import {Container, Card, Icon, Image} from "semantic-ui-react";
+
+import {Container, Card, Icon, Image, Grid, Segment} from "semantic-ui-react";
 
 const OperatorDashboard = (props) => {
   return (
-    <Container>
-      <h1>Operator Dashboard component</h1>
+    <>
+      <Header />
+      <Container fluid style={{padding: "2rem"}}>
+        <Grid columns={2}>
+          <Grid.Column computer={4} tablet={6} mobile={16}>
+            <Segment>
+              <Segment vertical textAlign="center">
+                <AddTruckForm />
+              </Segment>
 
-      <AddTruckForm />
-      {props.truck.map((t) => (
-        <Link to="/truck" key={t.truckName}>
-          <Card>
-            <Image
-              src={t.imageOfTruck}
-              alt={`${t.cuisineType} food truck`}
-              wrapped
-              ui={false}
-            />
-            <Card.Content>
-              <Card.Header>{t.truckName}</Card.Header>
+              <TruckList />
+            </Segment>
+          </Grid.Column>
 
-              <Card.Meta>
-                <span className="date">
-                  Date: {t.currentLocation.departureTime.date}
-                </span>
-                <span className="date">
-                  Time: {t.currentLocation.departureTime.time}
-                </span>
-              </Card.Meta>
-              <Card.Description>Cuisine: {t.cuisineType}</Card.Description>
-              <Card.Description>
-                Current Location: {t.currentLocation.location}
-              </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <Icon name="star" />
-              Average Rating: {t.customerRatingAvg}
-            </Card.Content>
-          </Card>
-        </Link>
-      ))}
-
-      <Switch>
-        <Route exact path="/truck" component={FoodTruck} />
-      </Switch>
-    </Container>
+          <Grid.Column computer={12} tablet={10} mobile={16}>
+            <FoodTruck />
+          </Grid.Column>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    truck: state.operator.truck,
-  };
-};
-
-export default connect(mapStateToProps)(OperatorDashboard);
+export default OperatorDashboard;
