ccbfad913d41f72b462d5e698349c94705ccce35
slroberts <shomariroberts@gmail.comclear>
2020-08-21 21:02:08 -0400

    changed the position of map()

--- src/components/operator/OperatorDashboard.js
+++ src/components/operator/OperatorDashboard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {connect} from "react-redux";
-// import {BrowserRouter as Switch, Route, Link} from "react-router-dom";
 
 import Header from "../../components/Header";
 import AddTruckForm from "./AddTruckForm";
@@ -14,23 +13,23 @@ const OperatorDashboard = (props) => {
     <>
       <Header />
       <Container fluid style={{padding: "2rem"}}>
-        <Grid columns={2}>
-          <Grid.Column computer={4} tablet={6} mobile={16}>
-            <Segment>
-              <Segment vertical textAlign="center">
-                <AddTruckForm />
+        {props.trucks.map((truck) => (
+          <Grid key={Date.now()} columns={2}>
+            <Grid.Column computer={4} tablet={6} mobile={16}>
+              <Segment>
+                <Segment vertical textAlign="center">
+                  <AddTruckForm />
+                </Segment>
+
+                <TruckList truck={truck} />
               </Segment>
+            </Grid.Column>
 
-              <TruckList />
-            </Segment>
-          </Grid.Column>
-
-          <Grid.Column computer={12} tablet={10} mobile={16}>
-            {props.trucks.map((truck) => (
-              <FoodTruck key={truck.id} truck={truck} />
-            ))}
-          </Grid.Column>
-        </Grid>
+            <Grid.Column computer={12} tablet={10} mobile={16}>
+              <FoodTruck truck={truck} />
+            </Grid.Column>
+          </Grid>
+        ))}
       </Container>
     </>
   );
