40fc13aea1a97b0bed8b255244f1cba0c594cc08
slroberts <shomariroberts@gmail.comclear>
2020-08-24 10:41:24 -0400

    added logout function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import {useHistory} from "react-router-dom";
 import {Menu, Icon} from "semantic-ui-react";
 
 const Header = () => {
+  const {push} = useHistory();
+  const handleLogout = () => {
+    localStorage.clear();
+    push("/");
+  };
+
   return (
     <>
       <Menu inverted size="massive" style={{borderRadius: 0}}>
@@ -12,7 +19,11 @@ const Header = () => {
             <Icon name="setting" />
           </Menu.Item>
           <Menu.Item>
-            <Icon name="log out" />
+            <Icon
+              name="log out"
+              onClick={handleLogout}
+              style={{cursor: "pointer"}}
+            />
           </Menu.Item>
         </Menu.Menu>
       </Menu>
