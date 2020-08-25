b1554daa2061ec9bac637fc7ede6d23a94b5662b
slroberts <shomariroberts@gmail.comclear>
2020-08-25 11:53:34 -0400

    updated Routes

--- src/App.js
+++ src/App.js
@@ -10,21 +10,26 @@ import OperatorLogin from "./components/operator/OperatorLogin";
 import DinerDashboard from "./components/diner/DinerDashboard";
 import OperatorDashboard from "./components/operator/OperatorDashboard";
 import ImageEditor from "./components/operator/ImageEditor";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <Router>
-      <Route exact path="/diner" component={DinerLogin} />
-      <Route exact path="/diner/home" component={DinerDashboard} />
-      <Route exact path="/operator" component={OperatorLogin} />
-      <Route exact path="/image-editor" component={ImageEditor} />
-      <Route exact path="/" >
-        <div className="App">Food Truck TrackR</div>
-        <NavLink to="/diner">Diner</NavLink>
-        <NavLink to="/operator">Operator</NavLink>
-        <NavLink to="/operator/dashboard">Operator Dashboard</NavLink>
-        <NavLink to="/diner/home">Diner Dashboard</NavLink>
-      </Route>
+      <div className="App">Food Truck TrackR</div>
+      <NavLink to="/diner">Diner</NavLink>
+      <NavLink to="/operator">Operator</NavLink>
+
+      <Switch>
+        <Route exact path="/diner" component={DinerLogin} />
+        <Route exact path="/operator" component={OperatorLogin} />
+        <PrivateRoute exact path="/diner/home" component={DinerDashboard} />
+        <PrivateRoute
+          exact
+          path="/operator/dashboard:operatorId"
+          component={OperatorDashboard}
+        />
+        <Route exact path="/image-editor" component={ImageEditor} />
+      </Switch>
     </Router>
   );
 };
